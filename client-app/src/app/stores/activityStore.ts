227f1f54f9f972e715eb59edca30ca7c7e579f14
Michael Lee <m.maurice.lee@gmail.com>
2019-12-14 23:48:38 -0700

    Added improved form and validation

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,9 @@ import { observable, action, computed, configure, runInAction } from 'mobx';
 import { createContext, SyntheticEvent } from 'react';
 import { IActivity } from '../models/activity';
 import agent from '../api/agent';
+import { history } from '../..';
+import { toast } from 'react-toastify';
+
 
 configure({ enforceActions: 'always' });
 
@@ -18,12 +21,12 @@ class ActivityStore {
 
     groupActivitiesByDate(activities: IActivity[]) {
         const sortedActivities = activities.sort((a, b) =>
-            Date.parse(a.date) - Date.parse(b.date))
+            a.date.getTime() - b.date.getTime())
         return Object.entries(sortedActivities.reduce((activities, activity) => {
-            const date = activity.date.split('T')[0];
+            const date = activity.date.toISOString().split('T')[0];
             activities[date] = activities[date] ? [...activities[date], activity] : [activity];
             return activities;
-        }, {} as {[key:string] : IActivity[]}));
+        }, {} as { [key: string]: IActivity[] }));
     }
 
     @action loadActivities = async () => {
@@ -32,7 +35,7 @@ class ActivityStore {
             const activities = await agent.Activities.list();
             runInAction('loading activities', () => {
                 activities.forEach((activity) => {
-                    activity.date = activity.date.split(".")[0];
+                    activity.date = new Date(activity.date!);
                     this.activityRegistry.set(activity.id, activity);
                 });
             });
@@ -52,19 +55,23 @@ class ActivityStore {
         let activity = this.getActivity(id);
         if (activity) {
             this.activity = activity;
+            return activity;
         } else {
             this.loadingInitial = true;
             try {
                 activity = await agent.Activities.details(id);
                 runInAction('getting activity', () => {
+                    activity.date = new Date(activity.date)
                     this.activity = activity;
+                    this.activityRegistry.set(activity.id, activity);
                     this.loadingInitial = false;
-                })
+                });
+                return activity;
             } catch (error) {
                 runInAction('get activity error', () => {
                     this.loadingInitial = false;
                 })
-                console.log(error);
+                console.log(error.response);
             }
         }
     }
@@ -83,15 +90,15 @@ class ActivityStore {
             await agent.Activities.create(activity);
             runInAction('creating activity', () => {
                 this.activityRegistry.set(activity.id, activity);
-            })
+                this.submitting = false;
+            });
+            history.push(`/activities/${activity.id}`);
         } catch (error) {
             runInAction('create activity error', () => {
-                console.log(error);
-            })
-        } finally {
-            runInAction('set submitting to false', () => {
                 this.submitting = false;
             })
+            toast.error('Problem submitting data');
+            console.log(error.response);
         }
     }
 
@@ -102,15 +109,14 @@ class ActivityStore {
             runInAction('editing activity', () => {
                 this.activityRegistry.set(activity.id, activity);
                 this.activity = activity;
-            })
+                this.submitting = false;
+            });
+            history.push(`/activities/${activity.id}`);
         } catch (error) {
             runInAction('edit activity error', () => {
-                console.log(error);
-            })
-        } finally {
-            runInAction('set submitting to false', () => {
                 this.submitting = false;
-            })
+            });
+            console.log(error);
         }
     }
 
